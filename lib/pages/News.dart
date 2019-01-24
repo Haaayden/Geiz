7ec10d054698b74759fb83360506ceb613f320f0
Haaayden <brooklyn1984@qq.com>
2019-01-24 10:20:49 +0800

    feat(News.dart): 增加ListView

--- lib/pages/News.dart
+++ lib/pages/News.dart
@@ -1,12 +1,30 @@
 import 'package:flutter/material.dart';
+import '../widgets/NewsCardCommon.dart';
 
-class News extends StatelessWidget {
+class News extends StatefulWidget {
   @override
-    Widget build(BuildContext context) {
-      return Container(
-        child: Center(
-          child: Text('News'),
-        ),
-      );
-    }
+  _NewsState createState() => _NewsState();
+}
+
+class _NewsState extends State {
+  @override
+  Widget build(BuildContext context) {
+    return RefreshIndicator(
+      color: Colors.yellow,
+      onRefresh: () {},
+      child: ListView.separated(
+        itemCount: 3,
+        separatorBuilder: (BuildContext context, int index) {
+          return Container(
+            height: 5,
+            color: Color.fromARGB(50, 183, 187, 197),
+          );
+        },
+        itemBuilder: (BuildContext context, int index) {
+          return NewsCardCommon();
+        },
+      ),
+    );
+  }
+
 }
---
+++ lib/widgets/NewsCardCommon.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class NewsCardCommon extends StatelessWidget {
+  @override
+    Widget build(BuildContext context) {
+      return Container(
+        height: 100.0,
+        color: Colors.blueGrey,
+        child: Center(
+          child: Text('NewsCradCommon'),
+        ),
+      );
+    }
+}
